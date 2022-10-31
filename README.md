dd376e4e6c00132d31ba21d4ef2816a52e395cf1
ruu.otsuka <ruu.otsuka@gmail.com>
2022-10-31 14:39:55 +0900

    aaaaaa

--- README.md
+++ README.md
@@ -4,6 +4,20 @@
 爆速でlaravel + docker + CRUD + nuxt.js環境を立てるtemplateブランチ
 
 [入ってるもの]
+・bootstrap-4.3.1
+## Quick start
+
+Several quick start options are available:
+
+- [Download the latest release](https://github.com/twbs/bootstrap/archive/v5.2.2.zip)
+- Clone the repo: `git clone https://github.com/twbs/bootstrap.git`
+- Install with [npm](https://www.npmjs.com/): `npm install bootstrap@v5.2.2`
+- Install with [yarn](https://yarnpkg.com/): `yarn add bootstrap@v5.2.2`
+- Install with [Composer](https://getcomposer.org/): `composer require twbs/bootstrap:5.2.2`
+- Install with [NuGet](https://www.nuget.org/): CSS: `Install-Package bootstrap` Sass: `Install-Package bootstrap.sass`
+
+Read the [Getting started page](https://getbootstrap.com/docs/5.2/getting-started/introduction/) for information on the framework contents, templates, examples, and more.
+
 
 
 [入れる予定]
---
+++ vendor/composer/installed.php
@@ -0,0 +1,38 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '00b0e21b48ac208471d76b9096b9dac59699778e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '00b0e21b48ac208471d76b9096b9dac59699778e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'twbs/bootstrap' => array(
+            'pretty_version' => 'v5.2.2',
+            'version' => '5.2.2.0',
+            'reference' => '961d5ff9844372a4e294980c667bbe7e0651cdeb',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twbs/bootstrap',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'twitter/bootstrap' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => 'v5.2.2',
+            ),
+        ),
+    ),
+);
