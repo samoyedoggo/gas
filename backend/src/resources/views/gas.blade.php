5f20a643d07df41003c4e8b872be3a19d3a4b632
ruu.otsuka <ruu.otsuka@gmail.com>
2022-10-31 13:44:21 +0900

    aaa

---
+++ backend/src/resources/views/gas.blade.php
@@ -0,0 +1,7 @@
+<x-app-layout>
+    <h1>RCDX</h1>
+        <p class="lead">RCシステムフォーム</p>
+        <p class="lead">
+        <a href="https://discord.gg/dPszxNKN8m" class="btn btn-lg btn-secondary fw-bold border-white bg-white">チャットルームへ</a>
+        </p>
+</x-app-layout>
